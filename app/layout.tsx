4d4c635d2f7780d6b2ccdefba0bf202e2cafa821
Caleb Jennings <calebjennings@Calebs-MacBook-Air.local>
2026-05-22 09:45:45 -0400

    next init

---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const nunito = Nunito({
+  variable: "--font-nunito",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={`${nunito.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <main>
+        <h1 className="text-9xl font-sans text-center">
+          Welcome to Cookie Stack!
+        </h1>
+      </main>
+    </div>
+  );
+}
