3e3cd741fa89073aa6a74bff6e9ccfa85fd3fc4f
Caleb Jennings <calebbirtch.j@gmail.com>
2026-05-22 19:24:27 -0400

    add animation to mount

--- app/idea/page.tsx
+++ app/idea/page.tsx
@@ -1,8 +1,20 @@
+"use client";
+
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Idea() {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center overflow-hidden p-4">
+    <div
+      className={`flex min-h-screen flex-col items-center justify-center overflow-hidden p-4 transition-all duration-500 ease-out ${
+        mounted ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"
+      }`}
+    >
       <main className="flex w-full items-center justify-around gap-4 text-center">
         <div className="flex w-full max-w-96 h-48 flex-col items-center justify-center border-2 rounded-2xl p-4 gap-4">
           <p className="text-4xl font-sans">Random Idea</p>
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ["10.0.0.178"],
 };
 
 export default nextConfig;
