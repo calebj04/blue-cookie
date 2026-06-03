54ac1aa4edf1024b6bd6038b3901061f160f7ff3
Caleb Jennings <calebbirtch.j@gmail.com>
2026-06-03 17:37:30 -0400

    initialize logic for calling gemini

--- app/new/idea/page.tsx
+++ app/new/idea/page.tsx
@@ -1,14 +1,78 @@
 "use client";
 
 import { useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import Random from "@/components/new-pages/Random";
+
+function generate(setLoading: (loading: boolean) => void) {
+  setTimeout(() => {
+    setLoading(false);
+  }, 2000);
+}
 
 export default function Idea() {
+  const [loading, setLoading] = useState(false);
+  const [input, setInput] = useState("");
+
   const searchParams = useSearchParams();
-  const idea = searchParams.get("idea");
+  const mode = searchParams.get("q");
+
+  useEffect(() => {
+    if (mode === "random") {
+      setLoading(true);
+      //prompt gemini
+      generate(setLoading);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (mode === "custom" && input) {
+      setLoading(true);
+      //prompt gemini
+      generate(setLoading);
+    }
+  }, [input]);
+
+  if (mode !== "random" && mode !== "custom") {
+    return (
+      <main className="flex w-full items-center justify-around gap-4 text-center">
+        no idea baby!
+      </main>
+    );
+  }
+
+  if (loading) {
+    return (
+      <main className="flex w-full items-center justify-around gap-4 text-center">
+        loading idea baby!
+      </main>
+    );
+  }
+
+  if (mode === "custom" && input === "") {
+    return (
+      <main className="flex w-full items-center justify-around gap-4 text-center">
+        <button
+          onClick={() => setInput("something")}
+          className="
+            border border-gray-400
+            bg-gray-100
+            px-2 py-1
+            rounded
+            text-black
+            hover:bg-gray-200
+            active:bg-gray-300
+  "
+        >
+          Click here to run `setInput("something")`
+        </button>
+      </main>
+    );
+  }
 
   return (
     <main className="flex w-full items-center justify-around gap-4 text-center">
-      new idea baby!
+      Your idea is beautiful!
     </main>
   );
 }
