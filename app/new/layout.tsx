ef13730675d0e12b381d4bccb3f12e06e2ff51e9
Caleb Jennings <calebbirtch.j@gmail.com>
2026-06-07 18:30:40 -0400

    wrap layout with providers

--- app/new/layout.tsx
+++ app/new/layout.tsx
@@ -1,9 +1,15 @@
 import { ReactNode } from "react";
+import IdeaProvider from "@/components/providers/IdeaProvider";
+import StatusProvider from "@/components/providers/StatusProvider";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-full flex flex-col animate-fade-in-up">
-      {children}
-    </div>
+    <StatusProvider initialStatus="start">
+      <IdeaProvider initialIdea="">
+        <div className="min-h-full flex flex-col animate-fade-in-up">
+          {children}
+        </div>
+      </IdeaProvider>
+    </StatusProvider>
   );
 }
