c01ff5b9d8009ee51e19bc716a305c39c66bcf22
Caleb Jennings <calebbirtch.j@gmail.com>
2026-06-09 17:56:00 -0400

    prompts baby

---
+++ lib/prompts.ts
@@ -0,0 +1,69 @@
+const random = `I am a CS student trying to learn with project based learning. Please generate 
+a random idea for a project and give me a roadmap of how to make it. I would like for you to 
+give me a simple description of the project, the main user flow (1-3 depending on the scope), 
+the tech stack, the milestones and sub issues (for GitHub), possible future features for 
+expansion (3-5), and the core concepts used/learned (5-7). Please respond in the following 
+JSON form:
+
+{
+  "description": "",
+  "user_flows": [
+    {
+      "name": "",
+      "steps": [
+        "",
+      ]
+    },
+  ],
+  "tech_stack": [
+    "",
+  ],
+  "milestones": [
+    {
+      "issues": [
+        ""
+      ]
+    },
+  ],
+  "future_features": [
+    ""
+  ],
+  "core_concepts": [
+    ""
+  ]
+}`;
+
+const custom = `I am a CS student trying to learn with project based learning. Please generate 
+an idea for a project based on the description or concepts provided and give me a roadmap of 
+how to make it. I would like for you to give me a simple description of the project, the main 
+user flow (1-3 depending on the scope), the tech stack, the milestones and sub issues (for 
+GitHub), possible future features for expansion (3-5), and the core concepts used/learned 
+(5-7). Please respond in the following JSON form:
+
+{
+  "description": "",
+  "user_flows": [
+    {
+      "name": "",
+      "steps": [
+        "",
+      ]
+    },
+  ],
+  "tech_stack": [
+    "",
+  ],
+  "milestones": [
+    {
+      "issues": [
+        ""
+      ]
+    },
+  ],
+  "future_features": [
+    ""
+  ],
+  "core_concepts": [
+    ""
+  ]
+}`;
