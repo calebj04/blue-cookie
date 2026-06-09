bcf084bb1a6a514d34be5e926bdf883581be35ac
Caleb Jennings <calebbirtch.j@gmail.com>
2026-06-08 23:01:59 -0400

    simple loading

--- components/new-pages/Loading.tsx
+++ components/new-pages/Loading.tsx
@@ -1,7 +1,7 @@
 export default function Loading() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center overflow-hidden p-4 animate-fade-in-up">
-      Loading...
+    <div className="flex min-h-screen items-center justify-center overflow-hidden p-4 animate-fade-in-up">
+      <h1 className="animate-pulse text-4xl">Loading...</h1>
     </div>
   );
 }
