b2bf4f5b587b446cfa69b59a43397fdb8a322cdd
Caleb Jennings <calebbirtch.j@gmail.com>
2026-06-09 21:31:35 -0400

    Prompt + stylize customize button

--- lib/prompts.ts
+++ lib/prompts.ts
@@ -1,9 +1,4 @@
-export const random = `I am a CS student trying to learn with project based learning. Please generate 
-a random idea for a project and give me a roadmap of how to make it. I would like for you to 
-give me a simple description of the project, the main user flow (1-3 depending on the scope), 
-the tech stack, the milestones and sub issues (for GitHub), possible future features for 
-expansion (3-5), and the core concepts used/learned (5-7). Please respond in the following 
-JSON form:
+export const random = `I am a CS student trying to learn with project based learning. Please generate a random idea for a project and give me a roadmap of how to make it. I would like for you to give me a simple description of the project, the main user flow (1-3 depending on the scope), the tech stack, the milestones and sub issues (for GitHub), possible future features for expansion (3-5), and the core concepts used/learned (5-7). Please respond in the following JSON form:
 
 {
   "description": "",
@@ -33,12 +28,7 @@ JSON form:
   ]
 }`;
 
-export const custom = `I am a CS student trying to learn with project based learning. Please generate 
-an idea for a project based on the description or concepts provided and give me a roadmap of 
-how to make it. I would like for you to give me a simple description of the project, the main 
-user flow (1-3 depending on the scope), the tech stack, the milestones and sub issues (for 
-GitHub), possible future features for expansion (3-5), and the core concepts used/learned 
-(5-7). Please respond in the following JSON form:
+export const custom = `I am a CS student trying to learn with project based learning. Please generate an idea for a project based on the following project description or concepts to learn provided below and give me a roadmap of how to make it. I would like for you to give me a simple description of the project, the main user flow (1-3 depending on the scope), the tech stack, the milestones and sub issues (for GitHub), possible future features for expansion (3-5), and the core concepts used/learned (5-7). Please respond in the following JSON form:
 
 {
   "description": "",
