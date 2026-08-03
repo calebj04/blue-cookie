071802bd01b5c50a18257d485dca5f114051ced7
Caleb Jennings <calebbirtch.j@gmail.com>
2026-08-03 10:53:41 -0400

    save provider token in local storage

--- components/providers/AuthProvider.tsx
+++ components/providers/AuthProvider.tsx
@@ -21,12 +21,38 @@ export default function AuthProvider({
 
   useEffect(() => {
     const supabase = createClient();
+    // Register this immediately after calling createClient!
+    // Because signInWithOAuth causes a redirect, you need to fetch the
+    // provider tokens from the callback.
 
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((event, session) => {
       setUser(session?.user ?? null);
+      console.log("Yes mommy?");
+
+      if (session && session.provider_token) {
+        window.localStorage.setItem(
+          "oauth_provider_token",
+          session.provider_token,
+        );
+      }
+
+      if (session && session.provider_refresh_token) {
+        window.localStorage.setItem(
+          "oauth_provider_refresh_token",
+          session.provider_refresh_token,
+        );
+      }
+
+      if (event === "SIGNED_OUT") {
+        window.localStorage.removeItem("oauth_provider_token");
+        window.localStorage.removeItem("oauth_provider_refresh_token");
+      }
     });
+
+    // Call this on your Sign in with GitHub button to initiate OAuth
+    // with GitHub with the requested elevated scopes.
 
     return () => {
       subscription.unsubscribe();
